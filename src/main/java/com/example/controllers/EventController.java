eca844ff7fc8e5e553691faf6f02c63a1d13fa1c
MaxCosta0 <maxleysoares006@gmail.com>
2021-01-07 15:21:01 -0300

    Saving data to the database

--- src/main/java/com/example/controllers/EventController.java
+++ src/main/java/com/example/controllers/EventController.java
@@ -1,13 +1,30 @@
 package com.example.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.models.Event;
+import com.example.repository.EventRepository;
 
 @Controller
 public class EventController {
 	
-	@RequestMapping(value = "/eventRegister")
+	@Autowired
+	private EventRepository ev;
+	
+	@RequestMapping(value="/eventRegister", method=RequestMethod.GET)
 	public String form() {
 		return "event/eventForm";
 	}
+	
+	@RequestMapping(value="/eventRegister", method=RequestMethod.POST)
+	public String form(Event event) {
+		
+		ev.save(event);
+		
+		return "redirect:/eventRegister";
+	}
+	
 }

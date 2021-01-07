104549bf574242ccfcad0dc95e6d35a0af991fc9
MaxCosta0 <maxleysoares006@gmail.com>
2021-01-07 00:09:41 -0300

    Database Configuration

--- src/main/java/com/example/eventapp/EventappApplication.java
+++ src/main/java/com/example/eventapp/EventappApplication.java
@@ -2,10 +2,12 @@ package com.example.eventapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example.controllers")
+@EntityScan("com.example.models")
 public class EventappApplication {
 
 	public static void main(String[] args) {
